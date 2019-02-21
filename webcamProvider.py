82aee7a6a044979df76fb1ba1df41d5790094efd
Simon <simon.taschke@googlemail.com>
2019-02-21 20:40:44 +0100

    fix webcam provider

--- webcamProvider.py
+++ webcamProvider.py
@@ -2,12 +2,17 @@ import redis
 import datetime
 import time
 import cv2
-
+import glob
+import json
+import os
 
 def webcam_provider():
-
     r = redis.Redis()
+    config = json.loads(r.get("webcam_config"))
+    image_interval = config["image_interval"]
+
     while True:
+        start_time = time.time()
         # Take image
         cap = cv2.VideoCapture(0)
         ret, frame = cap.read()
@@ -17,4 +22,10 @@ def webcam_provider():
         cv2.imwrite("web/" + image_name, frame)
         r.publish('snapshot', image_name)
         r.set('snapshot', image_name)
-        time.sleep(5)
+
+        image_list = glob.glob("picture*.jpg")
+        sorted_images = sorted(image_list, key=os.path.getmtime)
+
+        pause_time = (start_time + image_interval) - time.time()
+        if pause_time > 0:
+            time.sleep(pause_time)
