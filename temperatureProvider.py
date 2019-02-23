f853c847a32da5561d94d00a1f8a31aa83a5120c
Simon <simon.taschke@googlemail.com>
2019-02-23 14:07:17 +0100

    Simplify temperature reading

--- temperatureProvider.py
+++ temperatureProvider.py
@@ -2,41 +2,45 @@ import redis
 import random
 import json
 import time
-from sensor.ds20b18 import ds20b18
-
-r = redis.Redis()
 
 
-def get_temperature(sensors, virtual_sensor, emulate):
+def emulate_temperature(i):
+    return round(i * 5 + random.randint(0, 4) + random.uniform(0.0, 1.0), 1)
 
-    i = 0
-    temperature = {}
 
-    ## Read Sensordata
-    if emulate is True:
-        for i_sensor in sensors:
-            # Emulate Temperature
-            temperature[i_sensor["name"]] = round(i * 5 + random.randint(0, 4) + random.uniform(0.0, 1.0), 1)
-            i += 1
-    else:
-        for i_sensor in sensors:
-            temperature[i_sensor["name"]] = round(i_sensor.getTemperatur(), 1)
-
-    for i_virtual_sensor in virtual_sensor:
-        t = 0.0
-        for i_sensor in i_virtual_sensor["cummulate_sensors"]:
-            t += temperature[i_sensor]
-        temperature[i_virtual_sensor["name"]] = round(t/len(i_virtual_sensor["cummulate_sensors"]), 1)
-
-    r.publish('temperature', json.dumps(temperature))
-    r.set('temperature', json.dumps(temperature))
+def get_temperature(sensor_id):
+    # read temperature from file
+    file = open('/sys/bus/w1/devices/' + sensor_id + '/w1_slave')
+    filecontent = file.read()
+    file.close()
+    stringvalue = filecontent.split("\n")[1].split(" ")[9]
+    return round(float(stringvalue[2:]) / 1000, 1)
 
 
 def main():
+    r = redis.Redis()
     config = json.loads(r.get("temperature_config"))
 
     while True:
         start_time = time.time()
-        get_temperature(config["sensor"], config["virtual_sensor"], config["emulate_sensors"])
+        temperature = {}
+        if config["emulate_sensors"]:
+            i = 0
+            for sensor in config["sensor"]:
+                temperature[sensor["name"]] = emulate_temperature(i)
+                i += 1
+        else:
+            for sensor in config["sensor"]:
+                temperature[sensor["name"]] = get_temperature(sensor["serial"])
+
+        for i_virtual_sensor in config["virtual_sensor"]:
+            t = 0.0
+            for i_sensor in i_virtual_sensor["cummulate_sensors"]:
+                t += temperature[i_sensor]
+            temperature[i_virtual_sensor["name"]] = round(t / len(i_virtual_sensor["cummulate_sensors"]), 1)
+
+        r.publish('temperature', json.dumps(temperature))
+        r.set('temperature', json.dumps(temperature))
+
         delta_time = (start_time + config["measurement_intervall"]) - time.time()
         time.sleep(delta_time)
